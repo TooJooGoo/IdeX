3e8f7e86dfd4f4680656477e35d9ee74cacbf6d0
Tobias Göckel <tobiasgoeckel@hotmail.com>
2018-05-23 17:43:16 +0200

    Moved solution file.

--- Clean.bat
+++ Clean.bat
@@ -4,10 +4,13 @@
 set "cmx=call cmx "
 set thisDir=%~d0%~p0
 
+%cmx%Dir_Delete "%thisDir%\.vs"
+%cmx%Dir_Delete "%thisDir%\packages"
+
 %cmx%Dir_Delete "%thisDir%\Idex\bin"
 %cmx%Dir_Delete "%thisDir%\Idex\obj"
+%cmx%Dir_Delete "%thisDir%\Idex\.vs"
 
-%cmx%Dir_Delete "%thisDir%\.vs"
 %cmx%File_DeleteTree "%thisDir%\UpgradeLog*.htm"
 REM %cmx%File_DeleteTree "%thisDir%\*.csproj.bak"
 %cmx%File_DeleteTree "%thisDir%\*.csproj.user"
@@ -15,6 +18,6 @@ REM %cmx%File_DeleteTree "%thisDir%\*.nuget.props"
 REM %cmx%File_DeleteTree "%thisDir%\*.nuget.targets"
 REM %cmx%File_DeleteTree "%thisDir%\*.lock.json"
 
-%cmx%File_DeleteTree "%thisDir%\*.sln"
+REM %cmx%File_DeleteTree "%thisDir%\*.sln"
 
 
